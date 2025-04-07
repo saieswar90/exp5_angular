42978333adfcd1542b483a6053e5f6724965ba5b
Yoshrithasomalaraju <yoshrithav@gmail.com>
2025-04-07 22:27:35 +0530

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,26 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [RouterOutlet,CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'exp5';
+  title = 'Todo List with Angular'; 
+  list:any[]=[]; 
+  addtask(item:string) 
+  { 
+    this.list.push({id:this.list.length,name:item}) 
+    console.warn(this.list); 
+  } 
+  removeTask(id:number) 
+  { 
+    console.warn(id) 
+    this.list=this.list.filter(item=>item.id!==id); 
+  } 
 }
+
